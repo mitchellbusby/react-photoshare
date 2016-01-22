6cba5febb0a676cc6837a7cce887edfe9f1c0ed5
Mitchell Busby <mitchbusby@gmail.com>
2016-01-23 08:13:08 +1100

    Forgot to update actual function name for saving a token

--- src/actions/guest.js
+++ src/actions/guest.js
@@ -1,6 +1,6 @@
 import { SAVE_GUEST_TOKEN } from '../constants';
 
-export function generateToken(tokenToSave) {
+export function saveToken(tokenToSave) {
   localStorage.setItem('guest_token', tokenToSave);
   return {
     type: SAVE_GUEST_TOKEN,
