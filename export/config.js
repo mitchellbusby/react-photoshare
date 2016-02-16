a28a7078f060bc41cceef04788404d38363813f6
Mitchell Busby <mitchbusby@gmail.com>
2016-02-16 14:55:18 +1100

    Anonymises config file and adds it to git

---
+++ export/config.js
@@ -0,0 +1,12 @@
+// config.js
+var exports = module.exports = {};
+
+exports.awsAccessKey = process.env.AWS_ACCESS_KEY;
+exports.awsSecret = process.env.AWS_SECRET;
+exports.awsBucket = process.env.AWS_BUCKET;
+exports.awsRegion = process.env.AWS_REGION;
+exports.photosToUploadFolder = "";
+
+exports.mongoConnectionString = process.env.MONGO_CONNECTION_STRING;
+
+console.log(exports);
